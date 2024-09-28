a711d1f40088c398d1e00c86c93d3c3b8e9b175a
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-09-28 08:26:28 +0100

    added new file config.py and populated app.py and init.py

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
+from routes import app 
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'This is the home page'
+
+@app.route('/login')
+def login():
+    return ' This is the login page'
+
+@app.route('/sign-up')
+def sign_up():
+    return ' This is the sign-up page'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = ' dhuiqwhfudeiwf fewjfhiewf'
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'fdgdsfgeuwyfgbadjsfghduyjf'
+from routes import views
+
