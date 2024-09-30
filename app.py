0a2b52d6a89685638be10a2c65c3e2084a4e0c38
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-09-30 23:44:59 +0100

    added get/post method and validation to login / signup also added login manager

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from routes.forms import LoginForm, SignUpForm
-
+from flask_login import login_user, login_required, logout_user
+from flask_login import LoginManager
 
 from models import  Customer, Product, Cart, Order , db
 
@@ -13,7 +14,18 @@ app.config.from_object('config')
 with app.app_context():
      db.init_app(app)  
      db.create_all()
-     print('Database Created')  
+     print('Database Created') 
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'app.login'
+
+
+@login_manager.user_loader
+def load_user(id):
+        return Customer.query.get(int(id))
+
 
 
 
@@ -35,14 +47,67 @@ def women():
     return render_template('women-page.html')
 
 
-@app.route('/login')
+
+
+@app.route('/login',  methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        customer = Customer.query.filter_by(email=email).first()
+
+        if customer:
+            if customer.verify_password(password=password):
+                login_user(customer)
+                return redirect('/')
+            else:
+                flash('Incorrect Email or Password')
+
+        else:
+            flash('Account does not exist please Sign Up')
+
     return render_template('login.html', form=form)
 
-@app.route('/sign-up')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def log_out():
+    logout_user()
+    return redirect('/')
+
+    
+
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     form = SignUpForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        password1 = form.password1.data
+        password2 = form.password2.data
+
+        if password1 == password2:
+            new_customer = Customer()
+            new_customer.email = email
+            new_customer.username = username
+            new_customer.password = password2
+
+            try:
+                db.session.add(new_customer)
+                db.session.commit()
+                flash('Account Created Successfully, You can now Login')
+                return redirect('/login')
+            except Exception as e:
+                print(e)
+                flash('Account Not Created!!, Email already exists')
+
+            form.email.data = ''
+            form.username.data = ''
+            form.password1.data = ''
+            form.password2.data = ''
 
     return render_template('signup.html', form=form)
 
