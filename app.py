ac70643b873fa9ab37efbb65d9365ef553305c8f
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-10-01 19:17:28 +0100

    added delete-item route

--- app.py
+++ app.py
@@ -163,6 +163,25 @@ def update_item(item_id):
     return render_template('404.html')
 
 
+@app.route('/delete-item/<int:item_id>', methods=['GET', 'POST'])
+@login_required
+def delete_item(item_id):
+    if current_user.id == 1:
+        try:
+            item_to_delete = Product.query.get(item_id)
+            db.session.delete(item_to_delete)
+            db.session.commit()
+            flash('One Item deleted')
+            return redirect('/shop-items')
+        except Exception as e:
+            print('Item not deleted', e)
+            flash('Item not deleted!!')
+        return redirect('/shop-items')
+
+    return render_template('404.html')
+
+
+
 #LOGIN 
 
 @app.route('/login',  methods=['GET', 'POST'])
