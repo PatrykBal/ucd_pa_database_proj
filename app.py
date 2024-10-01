7f28ee6fd69ce30c39eb7875e2794ccc4506bdf2
Patryk Balecki <patrykbalecki@MACBOOKPRO-3F9F.station>
2024-10-01 21:23:41 +0100

    finished add-cart route and linked with men/women-page.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, send_from_directory
+from flask import Flask, render_template, flash, redirect, send_from_directory, request
 from routes.forms import LoginForm, SignUpForm, PasswordChangeForm, ShopItemsForm
 from flask_login import login_user, login_required, logout_user, current_user
 from flask_login import LoginManager
@@ -48,19 +48,49 @@ def men():
 
     items = Product.query.filter_by(flash_sale=True)
 
-    return render_template('men-page.html', items=items)
+    return render_template('men-page.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else [])
 
 @app.route('/women')
 def women():
 
     items = Product.query.filter_by(flash_sale=False)
 
-    return render_template('women-page.html', items=items)
+    return render_template('women-page.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else [])
 
 #ADD TO CART
 
-@app.route('/add-to-cart/int:item_id')
+@app.route('/add-to-cart/<int:item_id>')
 @login_required
+def add_to_cart(item_id):
+    item_to_add = Product.query.get(item_id)
+    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
+    if item_exists:
+        try:
+            item_exists.quantity = item_exists.quantity + 1
+            db.session.commit()
+            flash(f' Quantity of { item_exists.product.product_name } has been updated')
+            return redirect(request.referrer)
+        except Exception as e:
+            print('Quantity not Updated', e)
+            flash(f'Quantity of { item_exists.product.product_name } not updated')
+            return redirect(request.referrer)
+
+    new_cart_item = Cart()
+    new_cart_item.quantity = 1
+    new_cart_item.product_link = item_to_add.id
+    new_cart_item.customer_link = current_user.id
+
+    try:
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash(f'{new_cart_item.product.product_name} added to cart')
+    except Exception as e:
+        print('Item not added to cart', e)
+        flash(f'{new_cart_item.product.product_name} has not been added to cart')
+
+    return redirect(request.referrer)
 
 #ADMIN
 
